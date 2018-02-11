d2ae0987e482f70e2e22673444074f1b82116f94
yoshisman8 <4214215+yoshisman8@users.noreply.github.com>
2018-02-11 11:26:36 -0400

    why does .git hate me?

--- OracleBot/Classes/PlayerLocking.cs
+++ OracleBot/Classes/PlayerLocking.cs
@@ -1,8 +1,20 @@
+using System;
+using LiteDB;
+using System.Collections.Generic;
+using System.Linq;
+using OracleBot.Classes;
 
 namespace OracleBot.Classes
 {
     public class PlayerLocking
     {
+        
+    }
+
+    static class Lock{
+        public static void LockPayer(Character Character, ulong Owner, LiteDatabase Database){
+            
+        }
         
     }
 }
